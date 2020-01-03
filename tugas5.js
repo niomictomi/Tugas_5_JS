7f5ef04ce8de6ac95fed3b56be374f06e2db25dc
niomictomi <niomic.tomi@gmail.com>
2020-01-03 15:44:41 +0700

    Ouput3
    Output3

--- tugas5.js
+++ tugas5.js
@@ -1,11 +1,24 @@
 function tugas5 (){
     for (var i=1; i<=20; i++) {
-        if (i % 3 == 0) {
         console.log (i)
     }
 }
 
+function tugas5a() {
+    for (var i = 1; i <= 20; i++) {
+        
+
+        if (i % 3 == 0) {
+            console.log(i)
+        }
+    }
+
 
 }
+        
 
+console.log("Perulangan 20x")
 tugas5()
+console.log("")
+console.log("Perulangan Kelipatan 3")
+tugas5a()
